beb59a87359c539360b289d024ba87f4a72f9eeb
Packerbobohu <Packer@aliyun.com>
2026-05-14 16:11:09 +0800

    2605141611

--- functions/api/submit.js
+++ functions/api/submit.js
@@ -1,46 +1,5 @@
-export async function onRequestPost(context) {
+export function onRequest() {
 
-  const { env } = context;
-
-  try {
-
-    // 插入测试数据
-
-    const result = await env.DB.prepare(
-
-      `INSERT INTO customers
-      (name, phone)
-      VALUES (?, ?)`
-
-    )
-
-    .bind(
-
-      '张三',
-      '13800000000'
-
-    )
-
-    .run();
-
-    return Response.json({
-
-      success: true,
-
-      result
-
-    });
-
-  } catch(err) {
-
-    return Response.json({
-
-      success: false,
-
-      error: err.message
-
-    });
-
-  }
+  return new Response("hello functions");
 
 }
