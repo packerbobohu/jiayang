2dd1c8229a80f1bf75951bbb3bdaef02407f37c0
Packerbobohu <Packer@aliyun.com>
2026-05-15 09:56:24 +0800

    2605150956

--- functions/api/submit.js
+++ functions/api/submit.js
@@ -3,7 +3,6 @@ export async function onRequestPost(context) {
   const { request, env } = context;
 if (request.method === 'POST') {
   try {
-
     // 获取前端提交数据
     const formData = await request.json()
     // const data = await request.json();
@@ -14,6 +13,7 @@ if (request.method === 'POST') {
       service = formData.get('service');
       message = formData.get('message');
     // } = formData;
+console.log('Received POST request with body:', formData);
 
     // 写入 D1
 
